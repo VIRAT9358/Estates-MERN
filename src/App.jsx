ede9d4e5ef5febed1e94d57cd67971ad67a4cfa2
ViratSr <viratsrivastava17@gmail.com>
2023-11-07 13:43:48 +0530

    pages corrrected and links are added with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/Signin'
-import Signup from './pages/Signup'
-import About from './pages/About'
-import Profile from './pages/Profile'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home/>}/>    
-            <Route path='/SignIn' element={<SignIn/>}/>    
-            <Route path='/Signup' element={<Signup/>}/>    
-            <Route path='/About' element={<About/>}/>    
-            <Route path='/Profile' element={<Profile/>}/>    
-        
-        </Routes>
-    
-    
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
